7e55738ee92a46e1bf56cefae0210cef26119a27
Victoria Kabaeva <tori@ktori.me>
2020-04-16 08:25:43 +0300

    ci: fix array

--- bintray-descriptor.sh
+++ bintray-descriptor.sh
@@ -1,7 +1,7 @@
 CMAKE_CACHE=$(find . -type f -name "CMakeCache.txt")
 
-for PART in "MAJOR MINOR PATCH"
+for PART in MAJOR MINOR PATCH
 do
   VALUE=$(sed -n 's/CMAKE_PROJECT_VERSION_'"$PART"':STATIC=\(.*\)/\1/p' "$CMAKE_CACHE")
-  sed -i 's/VERSION_'"$PART"'/'"$VALUE"'/g' "descriptor.json"
+  echo sed -i 's/VERSION_'"$PART"'/'"$VALUE"'/g' "descriptor.json"
 done
